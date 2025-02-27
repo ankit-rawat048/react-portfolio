1362b6f4b9af6d784ac78aa445eb794a4a3bea80
ankit-rawat048 <ankitsinghrawt048@gmail.com>
2025-02-27 15:30:32 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes,Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
